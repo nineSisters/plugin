66e4b04e1152de8ddd2a6be3cf9c9cacd8ea6841
GitHub Action <action@github.com>
2021-03-30 14:36:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/ninesisters/plugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ninesisters/plugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ninesisters.plugin.services
 
+import com.github.ninesisters.plugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
